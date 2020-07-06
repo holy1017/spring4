2a4085bd8985c239ad139033ca2fd450558099dd
holy <holy1017@naver.com>
2020-07-06 21:32:50 +0900

    스프링부트4.4.7 초기 생성

---
+++ src/main/java/holy/HolyApplication.java
@@ -0,0 +1,13 @@
+package holy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/holy/HolyApplicationTests.java
@@ -0,0 +1,13 @@
+package holy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
