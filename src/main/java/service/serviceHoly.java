b2480f43ad3c14918917f25bdc8b1d4e7d36958d
holy star <holy1017@naver.com>
2020-07-07 15:25:47 +0900

    Create serviceHoly.java

---
+++ src/main/java/service/serviceHoly.java
@@ -0,0 +1,20 @@
+@service
+public class serviceHoly {
+
+privite string text1;
+
+public serviceHoly(String text1){
+this.text1=text1;
+}
+
+privite string text2;
+@autowired
+public void setText2(String s){
+ this.text2=s;
+}
+
+@autowried
+privite string text3;
+
+
+}
